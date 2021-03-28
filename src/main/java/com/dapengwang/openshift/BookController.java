5a8867b93e38fddafa1fa5c941da35ed2be285b9
Dapeng Wang <dapeng.wang@geegbay.com>
2021-03-28 13:27:39 +0200

    Support CRUD

--- src/main/java/com/dapengwang/openshift/BookController.java
+++ src/main/java/com/dapengwang/openshift/BookController.java
@@ -23,5 +23,33 @@ public class BookController {
         return StreamSupport.stream(repository.findAll().spliterator(), false)
                 .collect(Collectors.toList());
     }
+    
+    @PostMapping("")
+    public ResponseEntity<Book> createBooks(@RequestBody Book book) {
+        Book result = repository.save(book);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(result.getId())
+                .toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.LOCATION, location.toString()).body(result);
+    }
+
+    @GetMapping("/{id}")
+    public Book retrieveBook(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return repository.findById(id).map(current -> {
+            current.setName(book.getName());
+            repository.save(current);
+            return current;
+        }).orElse(null);
+    }
 }
 
