12c0d47e7256f562835086aca6fbc244cb51044a
Dapeng Wang <dapeng.wang@geegbay.com>
2021-03-30 13:32:56 +0200

    Start

--- src/main/java/com/dapengwang/openshift/BookController.java
+++ src/main/java/com/dapengwang/openshift/BookController.java
@@ -1,55 +1,16 @@
 package com.dapengwang.openshift;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
-    private final BookRepository repository;
-
     @GetMapping("")
-    public List<Book> retrieveAll() {
-        return StreamSupport.stream(repository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
-    }
-    
-    @PostMapping("")
-    public ResponseEntity<Book> createBooks(@RequestBody Book book) {
-        Book result = repository.save(book);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}").buildAndExpand(result.getId())
-                .toUri();
-        return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.LOCATION, location.toString()).body(result);
-    }
-
-    @GetMapping("/{id}")
-    public Book retrieveBook(@PathVariable Long id) {
-        return repository.findById(id).orElse(null);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable Long id) {
-        repository.deleteById(id);
-    }
-
-    @PatchMapping("/{id}")
-    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
-        return repository.findById(id).map(current -> {
-            current.setName(book.getName());
-            repository.save(current);
-            return current;
-        }).orElse(null);
+    public String retrieveAll() {
+        return "Hello World";
     }
 }
-
