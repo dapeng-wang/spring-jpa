9a112cfba03c626e96ebb63fe0d51dd840517a1f
Dapeng Wang <dapeng.wang@geegbay.com>
2021-03-30 13:53:21 +0200

    Add JPA features

---
+++ src/main/java/com/dapengwang/openshift/Book.java
@@ -0,0 +1,24 @@
+package com.dapengwang.openshift;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String author;
+}
--- src/main/java/com/dapengwang/openshift/BookController.java
+++ src/main/java/com/dapengwang/openshift/BookController.java
@@ -1,16 +1,33 @@
 package com.dapengwang.openshift;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
-public class BookController {
+public class BookController implements CommandLineRunner {
+
+    @Autowired
+    private BookRepository repository;
+
     @GetMapping("")
-    public String retrieveAll() {
-        return "Hello World";
+    public List<Book> retrieveAll() {
+        return StreamSupport.stream(repository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Book(null, "Java", "Gosling"));
+        repository.save(new Book(null, "Scala", "Odersky"));
+        repository.save(new Book(null, "Python", "Van"));
     }
 }
