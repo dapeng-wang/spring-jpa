e4d55634b18373e9e6b3d4d3bfca0e47d20aafcd
Dapeng Wang <dapeng.wang@geegbay.com>
2021-03-30 13:58:49 +0200

    CRUD

--- src/main/java/com/dapengwang/openshift/BookController.java
+++ src/main/java/com/dapengwang/openshift/BookController.java
@@ -3,10 +3,13 @@ package com.dapengwang.openshift;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -22,6 +25,34 @@ public class BookController implements CommandLineRunner {
     @GetMapping("")
     public List<Book> retrieveAll() {
         return StreamSupport.stream(repository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Book> createBooks(@RequestBody Book book) {
+        Book result = repository.save(book);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(result.getId())
+                .toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.LOCATION, location.toString()).body(result);
+    }
+
+    @GetMapping("/{id}")
+    public Book retrieveBook(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return repository.findById(id).map(current -> {
+            current.setName(book.getName());
+            repository.save(current);
+            return current;
+        }).orElse(null);
     }
 
     @Override
